71537d49de1355a9e19c507057f8c89586c5656e
Jose Ampuero <ampuerovillanueva@gmail.com>
2024-07-19 12:21:22 -0500

    Bugs

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ class TodoApp {
         this.lineThrough = 'line-through';
         this.LIST = [];
         this.id = 0;
-<<<<<<< HEAD
         this.MAX_LENGTH = 20; // Longitud máxima del nombre de la tarea
-=======
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
 
         this.init();
     }
@@ -33,7 +30,6 @@ class TodoApp {
     }
 
     cargarDesdeLocalStorage() {
-<<<<<<< HEAD
         const data = localStorage.getItem('TODO');
         if (data) {
             try {
@@ -45,13 +41,6 @@ class TodoApp {
                 this.LIST = [];
                 this.id = 0;
             }
-=======
-        let data = localStorage.getItem('TODO');
-        if (data) {
-            this.LIST = JSON.parse(data);
-            this.id = this.LIST.length;
-            this.cargarLista(this.LIST);
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         }
     }
 
@@ -71,7 +60,6 @@ class TodoApp {
         const REALIZADO = realizado ? this.check : this.uncheck;
         const LINE = realizado ? this.lineThrough : '';
 
-<<<<<<< HEAD
         const elemento = document.createElement('li');
         elemento.classList.add('task-item'); // Añadir una clase CSS para el contenedor de la tarea
         elemento.innerHTML = `
@@ -84,58 +72,32 @@ class TodoApp {
             <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
         `;
         this.lista.appendChild(elemento);
-=======
-        const elemento = `
-            <li id="elemento">
-                <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
-                <p class="text ${LINE}">${tarea} - ${fechaCreacion}</p>
-                <i class="fas fa-edit edit" data="editar" id="${id}"></i>
-                <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
-            </li>
-        `;
-        this.lista.insertAdjacentHTML("beforeend", elemento);
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
     }
 
     tareaRealizada(element) {
         element.classList.toggle(this.check);
         element.classList.toggle(this.uncheck);
-<<<<<<< HEAD
         const tareaTexto = element.parentNode.querySelector('.task-text');
         tareaTexto.classList.toggle(this.lineThrough);
-=======
-        element.parentNode.querySelector('.text').classList.toggle(this.lineThrough);
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         this.LIST[element.id].realizado = !this.LIST[element.id].realizado;
         localStorage.setItem('TODO', JSON.stringify(this.LIST));
     }
 
     tareaEliminada(element) {
-<<<<<<< HEAD
         element.parentNode.remove();
-=======
-        element.parentNode.parentNode.removeChild(element.parentNode);
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         this.LIST[element.id].eliminado = true;
         localStorage.setItem('TODO', JSON.stringify(this.LIST));
     }
 
     tareaEditada(element) {
-<<<<<<< HEAD
         const tareaTexto = element.parentNode.querySelector('.task-text');
         const tareaId = element.id;
         const tareaNombre = tareaTexto.querySelector('.task-name').textContent;
         const tareaFecha = tareaTexto.querySelector('.task-date').textContent;
-=======
-        const tareaTexto = element.parentNode.querySelector('.text');
-        const tareaId = element.id;
-        const [tareaNombre, tareaFecha] = tareaTexto.textContent.split(' - ');
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         const nuevoNombre = prompt('Edita la tarea:', tareaNombre);
         const nuevaFecha = prompt('Edita la fecha (dd/mm):', tareaFecha);
 
         if (nuevoNombre && nuevaFecha) {
-<<<<<<< HEAD
             if (!this.validarNombre(nuevoNombre)) {
                 alert('Nombre de tarea inválido. Asegúrate de que no sea un enlace, no contenga emojis, y tenga caracteres permitidos.');
             } else if (nuevoNombre.length > this.MAX_LENGTH) {
@@ -199,46 +161,17 @@ class TodoApp {
                 this.id++;
                 this.input.value = '';
             }
-=======
-            tareaTexto.textContent = `${nuevoNombre} - ${nuevaFecha}`;
-            this.LIST[tareaId].nombre = nuevoNombre;
-            this.LIST[tareaId].fechaCreacion = nuevaFecha;
-            localStorage.setItem('TODO', JSON.stringify(this.LIST));
-        }
-    }
-
-    agregarTareaDesdeInput() {
-        const tarea = this.input.value;
-        if (tarea) {
-            const fechaCreacion = new Date().toLocaleDateString('es-MX', { day: '2-digit', month: '2-digit' });
-            this.agregarTarea(tarea, this.id, false, false, fechaCreacion);
-            this.LIST.push({
-                nombre: tarea,
-                id: this.id,
-                realizado: false,
-                eliminado: false,
-                fechaCreacion: fechaCreacion
-            });
-            localStorage.setItem('TODO', JSON.stringify(this.LIST));
-            this.id++;
-            this.input.value = '';
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         }
     }
 
     agregarTareaConEnter(event) {
-<<<<<<< HEAD
         if (event.key === 'Enter') {
-=======
-        if (event.key == 'Enter') {
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
             this.agregarTareaDesdeInput();
         }
     }
 
     gestionarTarea(event) {
         const element = event.target;
-<<<<<<< HEAD
         const elementData = element.getAttribute('data');
 
         if (elementData === 'realizado') {
@@ -246,15 +179,6 @@ class TodoApp {
         } else if (elementData === 'eliminado') {
             this.tareaEliminada(element);
         } else if (elementData === 'editar') {
-=======
-        const elementData = element.attributes.data.value;
-
-        if (elementData == 'realizado') {
-            this.tareaRealizada(element);
-        } else if (elementData == 'eliminado') {
-            this.tareaEliminada(element);
-        } else if (elementData == 'editar') {
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
             this.tareaEditada(element);
         }
     }
@@ -282,10 +206,6 @@ class TodoApp {
     }
 
     mostrarPendientes() {
-<<<<<<< HEAD
-=======
-        this.limpiarBusqueda();
->>>>>>> 61f277ee1640e6e40dbc8f7c37266e456ffc7b4e
         const listaPendientes = this.LIST.filter(tarea => !tarea.realizado && !tarea.eliminado);
         this.cargarLista(listaPendientes);
     }
